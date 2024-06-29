1b79a4028666062fb46fe7049a5b2162198e29ce
zhangx567 <zhangxy567@gmail.com>
2024-06-29 18:43:56 +0800

    1

--- java/HelloJava/src/hj_package/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package hj_package;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("I LOVE JAVA");
-    }
-}
---
+++ java/ch1/HelloWorld.java
@@ -0,0 +1,8 @@
+class classname{
+	 public static void main(String[] args){
+	 	System.out.println("learn java l love free 中国");
+
+
+	 }
+
+}
